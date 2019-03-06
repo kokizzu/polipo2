13e18d1a33313a72345b600aa72eb9230eca9d9d
Silas Brown <silasb@silas.cambridge.grapeshot.co.uk>
2019-03-06 10:23:00 +0000

    2019: I'm still here, no security issues found yet

--- mingw.h
+++ mingw.h
@@ -1,7 +1,7 @@
 /*
 Copyright (c) 2006 by Dan Kennedy.
 Copyright (c) 2006 by Juliusz Chroboczek.
-Copyright (c) 2017 by Silas S. Brown
+Copyright (c) 2017-2019 by Silas S. Brown
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
