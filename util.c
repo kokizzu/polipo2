771e90894d52443bc7fd51d2b9a75e54a70cf97f
Silas S. Brown <ssb22@cam.ac.uk>
2020-03-30 11:02:52 +0100

    2020: I'm still here, no security issues found yet

--- util.c
+++ util.c
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2003-2007 by Juliusz Chroboczek
-Copyright (c) 2017-2019 by Silas S. Brown
+Copyright (c) 2017-2020 by Silas S. Brown
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
